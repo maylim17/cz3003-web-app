7c6776d2eee0ca2fb9e59b04b13d04f2b19b99d4
chianfern <chianfern@gmail.com>
2014-10-08 11:15:39 +0800

    form handling

--- events.php
+++ events.php
@@ -1,121 +1,144 @@
 <?php
-
-	getEvents(1,1);
+	/*
+	echo 'hi';
+	if (isset($_POST['type'])) {
+		echo 'hi';
+		if (isset($_POST['type'])) {
+			echo "You have selected ",$_POST['type'];
+		}
+	}		
+	//getEvents(1	,1);
+	*/
+	function getTestEvents ($requestedTypeId, $requestedTimePeriodInHour) {
+		$sampleArray = array (  
+		  'error' => 0,
+		  'events' => 
+		  array (
+			0 => 
+			array (
+			  'id' => 55,
+			  'eventType' => 'Dengue',
+			  'priority' => 1,
+			  'callingTime' => 1411106400000,
+			  'postalCode' => '11111',
+			  'location' => 'North',
+			  'callerPhone' => '111',
+			  'description' => 'Dengue again and again. ',
+			),
+			1 => 
+			array (
+			  'id' => 44,
+			  'eventType' => 'Dengue',
+			  'priority' => 1,
+			  'callingTime' => 1411102800000,
+			  'postalCode' => '11111',
+			  'location' => 'South',
+			  'callerPhone' => '111',
+			  'description' => 'Dengue again',
+			),
+			2 => 
+			array (
+			  'id' => 11,
+			  'eventType' => 'Dengue',
+			  'priority' => 1,
+			  'callingTime' => 1411099200000,
+			  'postalCode' => '11111',
+			  'location' => 'South',
+			  'callerPhone' => '111',
+			  'description' => 'Dengue here',
+			),
+		  ),
+		);
+		return $sampleArray;
+		//return parseArray($sampleArray);
+	}
 	
+	//Parse the array of a specific event type.
+	//Currently not in use.
+	function parseArray ($eventArray) {
+	
+		$NorthWest = 0;
+		$SouthWest = 0;
+		$NorthEast = 0;
+		$SouthEast = 0;
+		$CentralSingapore = 0;
+		
+		foreach ($eventArray['events'] as $element) {
+			switch ($element['location']) {
+				case 'NorthWest':
+					$NorthWest++;
+					break;
+				case 'SouthWest':
+					$SouthWest++;
+					break;
+				case 'NorthEast':
+					$NorthEast++;
+					break;
+				case 'SouthEast':
+					$SouthEast++;
+					break;
+				case 'CentralSingapore':
+					$CentralSingapore++;
+					break;
+			}		
+		}
+		
+		echo ("N,S,E,W,C:\n");
+		echo $NorthWest,' ',$NorthEast,' ',$SouthEast,' ',$SouthWest,' ',$CentralSingapore	;
+		
+		
+		$eventCount = array (
+			'North' => $North,
+			'South' => $South,
+			'East' => $East,
+			'West' => $West,
+			'Central' => $Central
+		);
+		
+		echo 'returning the array:';
+		var_dump ($eventCount);
+		return $eventCount;
+	}
+
+	// Connects to server and retrieves events 
+	// Returns the array
 	function getEvents ($requestedTypeId, $requestedTimePeriodInHour)
 	{
-	$url = 'http://localhost/eventTypes.php';
-	$data = array('typeId' => $requestedTypeId, 'timePeriodInHour' => $requestedTimePeriodInHour);
- 	$postdata = http_build_query($data);
+		$url = 'http://172.22.245.232:9000/events';
+		$data = array('typeID' => $requestedTypeId, 'timePeriodInHour' => $requestedTimePeriodInHour);
+		//$data = array('typeID' => 1, 'timePeriodInHour' => 1);
+		$postdata = http_build_query($data);
+		
+		// use key 'http' even if you send the request to https://...
+		$options = array('http' => array(
+			'method'  => 'POST',
+			'header' => 'Content-type: application/x-www-form-urlencoded\r\n',
+			'content' => $postdata
+		));
+		$context  = stream_context_create($options);
+		$result = file_get_contents($url, false, $context);
+		echo 'var_dump: ';
+		var_dump($result);
+		
+		//still need to decode?
+		$eventArray = json_decode($result, true);
+		echo 'var_dump: ';
+		var_dump($eventArray);
+	 
+		//Sample JSON:
+		//{"error":0,"events":[{"id":55,"eventType":"Dengue","priority":1,"callingTime":1411106400000,"postalCode":"11111","location":"SouthWest","callerPhone":"111","description":"Dengue again and again. "},{"id":44,"eventType":"Dengue","priority":1,"callingTime":1411102800000,"postalCode":"11111","location":"SouthWest","callerPhone":"111","description":"Dengue again"},{"id":11,"eventType":"Dengue","priority":1,"callingTime":1411099200000,"postalCode":"11111","location":"SouthWest","callerPhone":"111","description":"Dengue here"}]}
+		
+		
+		if ($eventArray['error']!=0) {
+			echo ('error in server response');
+			return null;
+		} else {
+			return $eventArray;
+		};
+		
+		//parseArray($eventArray);
 	
-	echo "postdata is: ", $postdata;
-	
-	// use key 'http' even if you send the request to https://...
-	$options = array('http' => array(
-		'method'  => 'POST',
-		'header' => 'Content-type: application/json',
-		'content' => $postdata
-	));
-	$context  = stream_context_create($options);
-	$result = file_get_contents($url, false, $context);
-	echo 'var_dump: ';
-	var_dump($result);
-	
-	//still need to decode?
-	$eventArray = json_decode($result, true);
-	echo 'var_dump: ';
-	var_dump($eventArray);
- 
-	//Sample JSON:
-	//{"error":0,"events":[{"id":55,"eventType":"Dengue","priority":1,"callingTime":1411106400000,"postalCode":"11111","location":"SouthWest","callerPhone":"111","description":"Dengue again and again. "},{"id":44,"eventType":"Dengue","priority":1,"callingTime":1411102800000,"postalCode":"11111","location":"SouthWest","callerPhone":"111","description":"Dengue again"},{"id":11,"eventType":"Dengue","priority":1,"callingTime":1411099200000,"postalCode":"11111","location":"SouthWest","callerPhone":"111","description":"Dengue here"}]}
-	// Create the sample array for testing parsing
-	$sampleArray = array (
-	  'error' => 0,
-	  'events' => 
-	  array (
-		0 => 
-		array (
-		  'id' => 55,
-		  'eventType' => 'Dengue',
-		  'priority' => 1,
-		  'callingTime' => 1411106400000,
-		  'postalCode' => '11111',
-		  'location' => 'North',
-		  'callerPhone' => '111',
-		  'description' => 'Dengue again and again. ',
-		),
-		1 => 
-		array (
-		  'id' => 44,
-		  'eventType' => 'Dengue',
-		  'priority' => 1,
-		  'callingTime' => 1411102800000,
-		  'postalCode' => '11111',
-		  'location' => 'South',
-		  'callerPhone' => '111',
-		  'description' => 'Dengue again',
-		),
-		2 => 
-		array (
-		  'id' => 11,
-		  'eventType' => 'Dengue',
-		  'priority' => 1,
-		  'callingTime' => 1411099200000,
-		  'postalCode' => '11111',
-		  'location' => 'South',
-		  'callerPhone' => '111',
-		  'description' => 'Dengue here',
-		),
-	  ),
-	);
-	
-	
-	$eventArray = $sampleArray; // for testing only, to be removed for actual http request
-	
-	if ($eventArray['error']!=0) {
-		echo ('error in server response');
 	}
 	
-	$North = 0;
-	$South = 0;
-	$East = 0;
-	$West = 0;
-	$Central = 0;
 	
-	foreach ($eventArray['events'] as $element) {
-		switch ($element['location']) {
-			case 'North':
-				$North++;
-				break;
-			case 'South':
-				$South++;
-				break;
-			case 'East':
-				$East++;
-				break;
-			case 'West':
-				$West++;
-				break;
-			case 'Central':
-				$Central++;
-				break;
-		}		
-	}
-	
-	echo ("N,S,E,W,C:\n");
-	echo $North,' ',$South,' ',$East,' ',$West,' ',$Central;
-	
-	
-	$eventCount = array (
-		'North' => $North,
-		'South' => $South,
-		'East' => $East,
-		'West' => $West,
-		'Central' => $Central
-	);
-	
-	echo 'returning the array:';
-	var_dump ($eventCount);
-	return $eventCount;
-	}
  ?>
---
+++ testmapphp.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <style type="text/css">
+      html, body { height: 100%; margin: 0; padding: 0; font-family: "Calibri";} 
+
+      .block { float: left;}
+
+      #user-input { position: relative; height: 100%; width: 20%;}
+
+      #options { position: absolute; top: 5%; left: 10%;}
+
+      .form {}
+
+      #map-canvas { height: 100%; width: 80%; margin: 0; padding: 0; }
+
+    </style>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script type="text/javascript"
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC-ZJTx4_Nc8kpK1rlErHiWVGjSxU1Sj14">
+    </script>
+
+    <!--
+	<script type="text/javascript">
+      function initialize() {
+        var mapOptions = {
+          center: { lat: 1.371232, lng: 103.802948},
+          zoom: 12
+        };
+        var map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+    </script>
+	-->
+<!-- 
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
+-->
+<!--
+    <script>
+var gmap = new google.maps.Map(document.getElementById('map-canvas'), {
+  disableDefaultUI: true,
+  keyboardShortcuts: false,
+  draggable: false,
+  disableDoubleClickZoom: true,
+  scrollwheel: false,
+  streetViewControl: false
+});
+
+var view = new ol.View({
+  // make sure the view doesn't go beyond the 22 zoom levels of Google Maps
+  maxZoom: 21
+});
+view.on('change:center', function() {
+  var center = ol.proj.transform(view.getCenter(), 'EPSG:3857', 'EPSG:4326');
+  gmap.setCenter(new google.maps.LatLng(center[1], center[0]));
+});
+view.on('change:resolution', function() {
+  gmap.setZoom(view.getZoom());
+});
+
+var vector = new ol.layer.Vector({
+  source: new ol.source.GeoJSON({
+    url: 'data/geojson/countries.geojson',
+    projection: 'EPSG:3857'
+  }),
+  style: new ol.style.Style({
+    fill: new ol.style.Fill({
+      color: 'rgba(255, 255, 255, 0.6)'
+    }),
+    stroke: new ol.style.Stroke({
+      color: '#319FD3',
+      width: 1
+    })
+  })
+});
+
+var olMapDiv = document.getElementById('olmap');
+var map = new ol.Map({
+  layers: [vector],
+  interactions: ol.interaction.defaults({
+    altShiftDragRotate: false,
+    dragPan: false,
+    rotate: false
+  }).extend([new ol.interaction.DragPan({kinetic: null})]),
+  target: olMapDiv,
+  view: view
+});
+view.setCenter([0, 0]);
+view.setZoom(1);
+
+olMapDiv.parentNode.removeChild(olMapDiv);
+gmap.controls[google.maps.ControlPosition.TOP_LEFT].push(olMapDiv);
+    </script>
+<!-- 
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@	
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
+-->
+
+<script type="text/javascript">
+    var map;
+    var elevator;
+    var marker;
+    var myOptions;
+
+    $(document).ready(function () {
+
+      // var map;
+      // var elevator;
+      myOptions = {
+          zoom: 12,
+          center: { lat: 1.371232, lng: 103.818948},
+          mapTypeId: 'terrain'
+      };
+      map = new google.maps.Map($('#map-canvas')[0], myOptions);
+
+    });
+
+    function populateWithMarkers (inputArray) {
+
+	  //alert();
+      //['Singapore 320115', 'description', priority-3], 
+      var events = [
+        {address: 'Singapore 320115', priority: 3}, 
+        {address: 'NTU Hall of Residence 7', priority: 3}, 
+        // ['Raffles City', 2],
+        // ['AMK Hub', 2],
+        // ['Singapore Changi Airport', 1],
+        // ['Singapore Turf Club', 2],
+        // ['Singapore Upper Pierce Reservoir', 1]
+      ];
+	  
+	alert(JSON.stringify(inputArray)); //works
+	alert(inputArray.error); //works
+	alert(inputArray.events["1"]["location"]); //works
+	
+
+      for (var x = 0; x < events.length; x++) {
+          
+          var p;
+          var latlng;
+
+          $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+events[x].address+'&sensor=false', null, function (data) {
+              p = data.results[0].geometry.location
+              latlng = new google.maps.LatLng(p.lat, p.lng);
+
+
+              marker = new google.maps.Marker({
+              position: latlng,
+              map: map
+              //title: events[x][0]
+              });
+
+              var contentString;
+              //description, priority
+               if (events[x].priority==1)
+                 contentString = "Severity: Mild";
+               else if  (events[x].priority==2)
+                 contentString = "Severity: Urgent";
+               else if  (events[x].priority==3)
+                 contentString = "Severity: Critical";
+               else
+                 contentString = "No data..";
+
+              var infoWindow = new google.maps.InfoWindow({
+                content: contentString
+              });
+
+              //add click event listener to marker which will open infoWindow          
+              google.maps.event.addListener(marker, 'click', function() {
+                infoWindow.open(map,marker); // click on marker opens info window 
+              });
+
+              });
+      }
+    }
+    
+</script>
+
+
+  </head>
+  <body>
+      <div class="block" id="user-input">
+        <div id="box">
+        <div id="options">
+          
+		  <!--
+          <div id="crisis-type">	
+            Please select crisis type(s):
+            <form class="form"> 
+              <input type="radio" name="type" value="Weather"/>Weather<br>
+              <input type="radio" name="type" value="Dengue">Dengue Fever<br>
+              <input type="radio" name="type" value="Fire">Fire Outbreak<br>
+              <input type="radio" name="type" value="Gas">Gas Leakage
+            </form>
+          </div>
+			-->
+			<div id="crisis-type">	
+			<?php 
+				if (!isset($_POST['type']) || $_POST['type']=='Weather') {
+					echo "Displaying Weather Map.";
+				} else {
+					echo "Displaying ",$_POST['type'], " cases in the last 24 hours.";
+				};
+				
+			?>
+            <br>
+			<br>
+			Please select crisis type:<br>
+            <form action = "testmapphp.php" method="POST">
+			  <input type="radio" name="type" value="Weather" >Weather<br>
+              <input type="radio" name="type" value="Dengue">Dengue Fever<br>
+              <input type="radio" name="type" value="Gas">Gas Leakage<br>
+			  <input type="radio" name="type" value="Traffic">Traffic Accident<br>
+			  <!--<input type="radio" name="type" value="Fire">Fire Outbreak<br>-->
+			  <input type="submit" name="submission" value="Submit"<br>
+			</form><br>
+            
+			<?php
+				if (isset($_POST['type']) && $_POST['type']!='Weather') {
+					$type = $_POST['type'];
+			
+					include	 'events.php';
+					$typeID = 0;
+					switch ($type) {
+						case 'Dengue':
+							$typeID = 1;
+							break;
+						case 'Gas':
+							$typeID = 2;
+							break;
+						case 'Traffic':
+							$typeID = 3;
+							break;
+						default:
+							break;
+					}
+					if ($typeID!=0) {
+						$events = getTestEvents($typeID, 24);	// for testing
+						//getEvents($typeID, 24);
+						var_dump($events);
+					//	echo '<script type="text/javascript">', 'populateWithMarkers();', '</script>';
+					}
+				}
+			?>	
+			
+			<script type="text/javascript">
+			var eventsData = <?php echo json_encode($events) ?>;
+			populateWithMarkers(eventsData);
+			</script>
+			
+			</div>
+			
+          <br><br>
+		  
+
+		  
+<!--
+          <div id="crisis-region">
+            Please select region(s):
+            <form class="form">
+              <input type="checkbox" name="region" value="North">North<br>
+              <input type="checkbox" name="region" value="South">South<br>
+              <input type="checkbox" name="region" value="Central">Central<br>
+              <input type="checkbox" name="region" value="East">East<br>
+              <input type="checkbox" name="region" value="West">West
+            </form>
+          </div>
+-->
+        </div>
+        </div>
+      </div>
+      <div class="block" id="map-canvas"></div>
+  </body>
+</html>
