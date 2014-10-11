d65e1ea7df72ba4aedca9209874c4f6670d2ca99
sriporna0505 <sriporna.mukherjee@newton-circus.com>
2014-10-12 03:06:47 +0800

    Minor change

--- eventTypes.php
+++ eventTypes.php
@@ -1,4 +1,5 @@
 <?php
+  function getEventTypesOnstart(){
   //$eventTypes = '{"error":0,"eventTypes":[{"id":1,"name":"Dengue"},{"id":2,"name":"Gas Leak"},{"id":3,"name":"Traffic Accident"}]}';
   $url = "http://172.22.245.232:9000/getEventTypes";
  
@@ -6,5 +7,8 @@
   echo $result;
   $decoded = json_decode($result, TRUE);
   var_dump($decoded);
+
+  return $result;
+}
  
 ?>
